ddd3b2f76faa7ec86645d10f92e273515f1024a5
Tiger Wang <tigerwang@outlook.com>
2023-02-23 23:19:11 +0000

    wip
    Signed-off-by: Tiger Wang <tigerwang@outlook.com>

---
+++ cmd/appManagementListAppStores.go
@@ -0,0 +1,89 @@
+/*
+Copyright © 2023 IceWhaleTech
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"text/tabwriter"
+
+	"github.com/IceWhaleTech/CasaOS-CLI/codegen/app_management"
+	"github.com/spf13/cobra"
+)
+
+// appManagementListAppStoresCmd represents the appManagementListAppStores command
+var appManagementListAppStoresCmd = &cobra.Command{
+	Use:   "app-stores",
+	Short: "list registered app stores",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		rootURL, err := rootCmd.PersistentFlags().GetString(FlagRootURL)
+		if err != nil {
+			return err
+		}
+
+		url := fmt.Sprintf("http://%s/%s", rootURL, BasePathAppManagement)
+
+		client, err := app_management.NewClientWithResponses(url)
+		if err != nil {
+			return err
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), DefaultTimeout)
+		defer cancel()
+
+		response, err := client.AppStoreListWithResponse(ctx)
+		if err != nil {
+			return err
+		}
+
+		if response.StatusCode() != http.StatusOK {
+			var baseResponse app_management.BaseResponse
+			if err := json.Unmarshal(response.Body, &baseResponse); err != nil {
+				return fmt.Errorf("%s - %s", response.Status(), response.Body)
+			}
+
+			return fmt.Errorf("%s - %s", response.Status(), *baseResponse.Message)
+		}
+
+		w := tabwriter.NewWriter(cmd.OutOrStdout(), 0, 0, 3, ' ', 0)
+		defer w.Flush()
+
+		fmt.Fprintln(w, "ID\tURL")
+		fmt.Fprintln(w, "--\t---")
+
+		for id, appStore := range *response.JSON200.Data {
+			fmt.Fprintf(w, "%d\t%s\n", id, *appStore.URL)
+		}
+
+		return nil
+	},
+}
+
+func init() {
+	appManagementListCmd.AddCommand(appManagementListAppStoresCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// appManagementListAppStoresCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// appManagementListAppStoresCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
--- cmd/appManagementListApps.go
+++ cmd/appManagementListApps.go
@@ -28,9 +28,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// appManagementListLocalCmd represents the appManagementListLocal command
-var appManagementListLocalCmd = &cobra.Command{
-	Use:   "local",
+// appManagementListAppsCmd represents the appManagementListApps command
+var appManagementListAppsCmd = &cobra.Command{
+	Use:   "apps",
 	Short: "list locally installed apps",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		rootURL, err := rootCmd.PersistentFlags().GetString(FlagRootURL)
@@ -120,17 +120,17 @@ var appManagementListLocalCmd = &cobra.Command{
 }
 
 func init() {
-	appManagementListCmd.AddCommand(appManagementListLocalCmd)
+	appManagementListCmd.AddCommand(appManagementListAppsCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// appManagementListLocalCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// appManagementListAppsCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// appManagementListLocalCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// appManagementListAppsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 func appList(composeApp interface{}) (string, map[string]app_management.AppStoreInfo, error) {
